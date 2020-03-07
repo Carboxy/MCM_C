ec332c8cc840e10f2644acab00e0d1b2f680ed57
Kenneth <yancheng1112@sjtu.edu.cn>
2020-03-07 14:37:14 +0800

    update reputation.py

---
+++ reputation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Reputation:
+    def __init__(self, path="scoreboard/hair_dryer_score.csv"):
+        self.raw_df = pd.read_csv(path)
+
+    def _star2score(self, star):
+        '''
+            turn number of stars into score
+            1: 20, 2: 40, 3: 60, 4: 80, 5: 100
+        '''
+        return 20 * star
+
+    def draw(self, product_parent):
+        '''
+            By given product, analyse the relation between time and
+            total reputation, and draw the graph in the end
+        '''
+        df = self.raw_df.groupby("product_parent").get_group(product_parent)
+
+        # since raw data is sorted by time, we don't need to sort again,
+        # but we need to traverse reversedly
+
+        total_reputation = 0
+        total_score = 0
+        x_axis = []
+        y_axis = []
+        for idx in reversed(df.index):
+            score = df.score[idx]
+            total_score += score
+            total_reputation += self._star2score(df.star_rating[idx]) * score
+            x_axis.append(df.review_date[idx])
+            y_axis.append(total_reputation / total_score)
+
+        plt.title("Product %d Reputation Tendency" %(product_parent))
+        plt.xlabel("Time")
+        plt.ylabel("Reputation")
+        plt.plot(x_axis, y_axis)
+        plt.show()
+
+if __name__ == "__main__":
+    r = Reputation()
+    r.draw(16483457)
--- score.py
+++ score.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from lda_util import clean_review
 
 # REVISE ALL PARAMETERS HERE!
